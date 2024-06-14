554213855879993469ccc07a69c5ad2424e3f103
minhhai1302 <yasuo2018123@gmail.com>
2024-06-14 16:22:35 +0700

    Lab8

---
+++ Bai1/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Bai1
+{
+    internal class Program
+    {
+        static Random random = new Random();
+        static object lockObj = new object();
+        static int number;        
+        static void Thread1()
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                lock (lockObj)
+                {
+                    number = random.Next(1, 11);
+                    Console.WriteLine("Thead1: Số ngẫu nhiên: " + number);
+                    Monitor.Pulse(lockObj);
+                    Monitor.Wait(lockObj);
+                    Thread.Sleep(2000);
+                }                
+            }            
+        }
+
+        static void Thread2()
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                lock (lockObj)
+                {
+                    Monitor.Wait(lockObj);
+                    int squaredNumber = number * number;
+                    Console.WriteLine("Thread2: Bình phương của số: " + squaredNumber);
+                    Monitor.Pulse(lockObj);
+                    Thread.Sleep(1000);
+                }                
+            }
+        }
+
+        static void Main()
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            Thread thread1 = new Thread(Thread1);
+            Thread thread2 = new Thread(Thread2);
+
+            thread1.Start();
+            thread2.Start();
+            thread1.Join();
+            thread2.Join();
+
+
+            Console.WriteLine("Kết thúc chương trình.");
+            Console.ReadLine();
+        }        
+    }
+}
---
+++ Bai2/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Bai2
+{
+    internal class Program
+    {
+        static void Thread1FuntionLab8()
+        {
+            lock (lock1)
+            {
+                Console.WriteLine("Thead 1 locked lock 1");
+                Thread.Sleep(100);
+                Console.WriteLine("Thead 1 is waiting for lock 2");
+                lock (lock2)
+                {
+                    Console.WriteLine("Thead 1 locked lock 2");
+                }
+            }
+        }
+        static void Thread2FuntionLab8()
+        {
+            lock (lock2)
+            {
+                Console.WriteLine("Thead 2 locked lock 2");
+                Thread.Sleep(100);
+                Console.WriteLine("Thead 2 is waiting for lock 1");
+                lock (lock1)
+                {
+                    Console.WriteLine("Thead 2 locked lock 1");
+                }
+            }
+        }
+        public void bai2()
+        {
+            Thread thread1 = new Thread(Thread1FuntionLab8);
+            Thread thread2 = new Thread(Thread2FuntionLab8);
+            thread1.Start();
+            thread2.Start();
+            thread1.Join();
+            thread2.Join();
+            Console.WriteLine("end start");
+        }
+        static object lock1 = new object();
+        static object lock2 = new object();
+        static void Main(string[] args)
+        {
+            Program program = new Program();
+            program.bai2();
+            Console.ReadLine();
+        }
+    }
+}
